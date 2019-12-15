9f2614c8ae59f80dbc70e42ad53fb1af8691ede0
Wen Guan <wguan.icedew@gmail.com>
2019-12-15 15:20:54 +0100

    update base agent and constants

--- common/lib/idds/common/constants.py
+++ common/lib/idds/common/constants.py
@@ -44,43 +44,42 @@ class HTTP_STATUS_CODE:
     InternalError = 500
 
 
-class RequesterType(Enum):
-    Derivation = 0
-    EventStreaming = 1
-    Other = 99
-
-
 class RequestStatus(Enum):
     New = 0
     Transforming = 1
-    Transporting = 2
-    Processing = 3
-    Finished = 4
-    Failed = 5
-    Cancel = 6
-    ReQueue = 7
+    Finished = 2
+    SubFinished = 3
+    Failed = 4
+    Extend = 5
+    ToCancel = 6
+    Cancelling = 7
+    Cancelled = 8
 
 
 class RequestType(Enum):
     Derivation = 0
     EventStreaming = 1
+    StageIn = 2
     Other = 99
 
 
 class TransformType(Enum):
     Derivation = 0
     EventStreaming = 1
+    StageIn = 2
     Other = 99
 
 
 class TransformStatus(Enum):
     New = 0
     Transforming = 1
-    Transporting = 2
-    Processing = 3
-    Finished = 4
-    Failed = 5
-    Cancel = 6
+    Finished = 2
+    SubFinished = 3
+    Failed = 4
+    Extend = 5
+    ToCancel = 6
+    Cancelling = 7
+    Cancelled = 8
 
 
 class CollectionType(Enum):
@@ -97,8 +96,13 @@ class CollectionRelationType(Enum):
 
 class CollectionStatus(Enum):
     New = 0
-    Open = 1
-    Closed = 2
+    Updated = 1
+    Updated1 = 2
+    Open = 3
+    Closed = 4
+    SubClosed = 5
+    Failed = 6
+    Deleted = 7
 
 
 class ContentType(Enum):
@@ -110,9 +114,10 @@ class ContentStatus(Enum):
     New = 0
     Processing = 1
     Available = 2
-    Lost = 3
-    Deleted = 4
-    Failed = 5
+    Failed = 3
+    FinalFailed = 4
+    Lost = 5
+    Deleted = 6
 
 
 class GranularityType(Enum):
