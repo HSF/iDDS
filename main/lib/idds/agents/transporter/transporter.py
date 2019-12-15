afe0fb4d7e56d1cd1075a96b5080b803de409ffe
Wen Guan <wguan.icedew@gmail.com>
2019-12-15 15:19:59 +0100

    update transporter

--- main/lib/idds/agents/transporter/transporter.py
+++ main/lib/idds/agents/transporter/transporter.py
@@ -9,12 +9,12 @@
 # - Wen Guan, <wen.guan@cern.ch>, 2019
 
 import copy
+import traceback
 import Queue
-
 
 from idds.common.constants import (Sections, CollectionRelationType, CollectionStatus,
                                    ContentType, ContentStatus)
-from idds.common.exceptions import AgentPluginError
+from idds.common.exceptions import AgentPluginError, IDDSException
 from idds.common.utils import setup_logging
 from idds.core import collections as core_collections, contents as core_contents
 from idds.agents.common.baseagent import BaseAgent
@@ -30,7 +30,8 @@ class Transporter(BaseAgent):
     def __init__(self, num_threads=1, **kwargs):
         super(Transporter, self).__init__(num_threads=num_threads, **kwargs)
         self.config_section = Sections.Transporter
-        self.processed_queue = Queue.Queue()
+        self.processed_input_queue = Queue.Queue()
+        self.processed_output_queue = Queue.Queue()
 
     def get_new_input_collections(self):
         """
@@ -64,7 +65,7 @@ class Transporter(BaseAgent):
 
     def process_input_collection(self, coll):
         """
-        Process request
+        Process input collection
         """
         coll_metadata = self.get_collection_metadata(coll['scope'], coll['name'])
         contents = self.get_contents(coll['scope'], coll['name'])
@@ -89,24 +90,94 @@ class Transporter(BaseAgent):
                     'contents': new_contents}
         return new_coll
 
-    def finish_processed_input_collections(self):
-        while not self.processed_queue.empty():
-            coll = self.processed_queue.get()
-            self.logger.info("Main thread finished processing collection(%s) with number of contents: %s" % (coll['coll_id'], len(coll['contents'])))
-            parameter = copy.deepcopy(coll)
-            del parameter['contents']
-            core_collections.update_collection(**parameter)
+    def finish_processing_input_collections(self):
+        while not self.processed_input_queue.empty():
+            coll = self.processed_input_queue.get()
+            self.logger.info("Main thread finished processing intput collection(%s) with number of contents: %s" % (coll['coll_id'], len(coll['contents'])))
+            parameters = copy.deepcopy(coll)
+            del parameters['contents']
+            del parameters['coll_id']
+            core_collections.update_collection(coll_id=coll['coll_id'], parameters=parameters)
             core_contents.add_contents(coll['contents'])
+
+    def get_new_output_collections(self):
+        """
+        Get new output collections
+        """
+        coll_status = [CollectionStatus.Updated]
+        colls = core_collections.get_collections_by_status(status=coll_status,
+                                                           relations_type=CollectionRelationType.Output,
+                                                           time_period=1800)
+        self.logger.info("Main thread get %s [Updated] output collections to process" % len(colls))
+        return colls
+
+    def register_contents(self, scope, name, contents):
+        if 'contents_register' not in self.plugins:
+            raise AgentPluginError('Plugin contents_register is required')
+        return self.plugins['contents_register'](scope, name, contents)
+
+    def process_output_collection(self, coll):
+        """
+        Process output collection
+        """
+        if 'to_register' in coll['coll_metadata'] and coll['coll_metadata']['to_register'] is True:
+            contents = core_contents.get_contents(coll_id=coll['coll_id'])
+            registered_contents = self.get_contents(coll['scope'], coll['name'])
+            registered_content_names = [con['name'] for con in registered_contents]
+            new_contents = []
+            for content in contents:
+                if content['name'] not in registered_content_names:
+                    new_contents.append(content)
+            if new_contents:
+                self.register_contents(coll['scope'], coll['name'], new_contents)
+
+        coll['coll_status'] = CollectionStatus.Updated1
+        return coll
+
+    def finish_processing_output_collections(self):
+        while not self.processed_output_queue.empty():
+            coll = self.processed_output_queue.get()
+            self.logger.info("Main thread finished processing output collection(%s) with number of contents: %s" % (coll['coll_id']))
+            parameters = {'status': coll['coll_status']}
+            core_collections.update_collection(coll_id=coll['coll_id'], parameters=parameters)
 
     def prepare_finish_tasks(self):
         """
         Prepare tasks and finished tasks
         """
-        self.finish_processed_input_collections()
+        self.finish_processing_input_collections()
+        self.finish_processing_output_collections()
 
         colls = self.get_new_input_collections()
         for coll in colls:
-            self.submit_task(self.process_input_collection, self.processed_queue, coll)
+            self.submit_task(self.process_input_collection, self.processed_input_queue, coll)
+
+        colls = self.get_new_output_collections()
+        for coll in colls:
+            self.submit_task(self.process_output_collection, self.processed_output_queue, coll)
+
+    def run(self):
+        """
+        Main run function.
+        """
+        try:
+            self.logger.info("Starting main thread")
+
+            self.load_plugins()
+
+            for i in range(self.num_threads):
+                self.executors.submit(self.run_tasks, i)
+
+            while not self.graceful_stop.is_set():
+                try:
+                    self.prepare_finish_tasks()
+                    self.sleep_for_tasks()
+                except IDDSException as error:
+                    self.logger.error("Main thread IDDSException: %s" % str(error))
+                except Exception as error:
+                    self.logger.critical("Main thread exception: %s\n%s" % (str(error), traceback.format_exc()))
+        except KeyboardInterrupt:
+            self.stop()
 
 
 if __name__ == '__main__':
