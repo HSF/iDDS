3b46518c048a80cb5fd53ba3af5c9d55a7674053
Wen Guan <wguan.icedew@gmail.com>
2020-04-19 19:32:13 +0200

    update activelearning condor submitter and poller

--- atlas/lib/idds/atlas/processing/activelearning_condor_poller.py
+++ atlas/lib/idds/atlas/processing/activelearning_condor_poller.py
@@ -12,22 +12,17 @@
 """
 Class of activelearning condor plubin
 """
-import os
-import json
 import traceback
 
 
 from idds.common import exceptions
 from idds.common.constants import ContentStatus, ProcessingStatus
-from idds.common.utils import run_command
-from idds.atlas.processing.base_plugin import ProcessingPluginBase
+from idds.atlas.processing.condor_poller import CondorPoller
 
 
-class ActiveLearningCondorPoller(ProcessingPluginBase):
+class ActiveLearningCondorPoller(CondorPoller):
     def __init__(self, workdir, **kwargs):
-        super(ActiveLearningCondorPoller, self).__init__(**kwargs)
-        self.workdir = workdir
-        self.name = 'condor'
+        super(ActiveLearningCondorPoller, self).__init__(workdir, **kwargs)
 
     def __call__(self, processing, transform, input_collection, output_collection, output_contents):
         try:
@@ -97,71 +92,3 @@ class ActiveLearningCondorPoller(ProcessingPluginBase):
             self.logger.error(ex)
             self.logger.error(traceback.format_exc())
             raise exceptions.AgentPluginError('%s: %s' % (str(ex), traceback.format_exc()))
-
-    def get_job_dir(self, processing_id):
-        job_dir = 'processing_%s' % processing_id
-        job_dir = os.path.join(self.workdir, job_dir)
-        if not os.path.exists(job_dir):
-            os.makedirs(job_dir)
-        return job_dir
-
-    def poll_job_status(self, processing_id, job_id):
-        # 0 Unexpanded     U
-        # 1 Idle           I
-        # 2 Running        R
-        # 3 Removed        X
-        # 4 Completed      C
-        # 5 Held           H
-        # 6 Submission_err E
-        cmd = "condor_q -format '%s' ClusterId  -format ' %s' Processing_id -format ' %s' JobStatus " + str(job_id)
-        status, output, error = run_command(cmd)
-        self.logger.debug("poll job status: %s" % cmd)
-        self.logger.debug("status: %s, output: %s, error: %s" % (status, output, error))
-        if status == 0 and len(output) == 0:
-            cmd = "condor_history -format '%s' ClusterId  -format ' %s' Processing_id -format ' %s' JobStatus " + str(job_id)
-            status, output, error = run_command(cmd)
-            self.logger.debug("poll job status: %s" % cmd)
-            self.logger.debug("status: %s, output: %s, error: %s" % (status, output, error))
-
-        ret_err = None
-        if status == 0:
-            lines = output.split('\n')
-            for line in lines:
-                c_job_id, c_processing_id, c_job_status = line.split(' ')
-                if str(c_job_id) != str(job_id):
-                    continue
-
-                c_processing_id = int(c_processing_id)
-                c_job_status = int(c_job_status)
-                if c_processing_id != processing_id:
-                    final_job_status = ProcessingStatus.Failed
-                    ret_err = 'jobid and the processing_id mismatched'
-                else:
-                    job_status = c_job_status
-                    if job_status < 2:
-                        final_job_status = ProcessingStatus.Submitted
-                    elif job_status == 2:
-                        final_job_status = ProcessingStatus.Running
-                    elif job_status == 3:
-                        final_job_status = ProcessingStatus.Cancel
-                    elif job_status == 4:
-                        final_job_status = ProcessingStatus.Finished
-                    else:
-                        final_job_status = ProcessingStatus.Failed
-        else:
-            final_job_status = ProcessingStatus.Submitted
-        return final_job_status, ret_err
-
-    def parse_job_outputs(self, processing_id, output_json):
-        job_dir = self.get_job_dir(processing_id)
-        full_output_json = os.path.join(job_dir, output_json)
-        if not os.path.exists(full_output_json):
-            return None, '%s is not created' % str(output_json)
-        else:
-            try:
-                with open(full_output_json, 'r') as f:
-                    data = f.read()
-                outputs = json.loads(data)
-                return outputs, None
-            except Exception as ex:
-                return None, 'Failed to load the content of %s: %s' % (str(output_json), str(ex))
--- atlas/lib/idds/atlas/processing/activelearning_condor_submitter.py
+++ atlas/lib/idds/atlas/processing/activelearning_condor_submitter.py
@@ -12,22 +12,20 @@
 """
 Class of activelearning condor plubin
 """
+import json
 import os
 import traceback
 
 
 from idds.common import exceptions
 from idds.common.constants import ProcessingStatus
-from idds.common.utils import run_command
-from idds.atlas.processing.base_plugin import ProcessingPluginBase
+from idds.atlas.processing.condor_submitter import CondorSubmitter
 from idds.core import (catalog as core_catalog)
 
 
-class ActiveLearningCondorSubmitter(ProcessingPluginBase):
+class ActiveLearningCondorSubmitter(CondorSubmitter):
     def __init__(self, workdir, **kwargs):
-        super(ActiveLearningCondorSubmitter, self).__init__(**kwargs)
-        self.workdir = workdir
-        self.name = 'condor'
+        super(ActiveLearningCondorSubmitter, self).__init__(workdir, **kwargs)
 
     def __call__(self, processing, transform, input_collection, output_collection):
         try:
@@ -38,14 +36,21 @@ class ActiveLearningCondorSubmitter(ProcessingPluginBase):
                 files.append(file)
 
             input_list = ','.join(files)
-            sandbox = transform['transform_metadata']['sandbox']
+            job_dir = self.get_job_dir(processing['processing_id'])
+            input_json = 'idds_input.json'
+            with open(os.path.join(job_dir, input_json), 'w') as f:
+                json.dump(files, f)
+
+            sandbox = None
+            if 'sandbox' in transform['transform_metadata']:
+                sandbox = transform['transform_metadata']['sandbox']
             executable = transform['transform_metadata']['executable']
             arguments = transform['transform_metadata']['arguments']
             output_json = None
             if 'output_json' in transform['transform_metadata']:
                 output_json = transform['transform_metadata']['output_json']
 
-            job_id, outputs = self.submit_job(processing['processing_id'], sandbox, executable, arguments, input_list, output_json)
+            job_id, outputs = self.submit_job(processing['processing_id'], sandbox, executable, arguments, input_list, input_json, output_json)
 
             processing_metadata = processing['processing_metadata']
             processing_metadata['job_id'] = job_id
@@ -63,93 +68,3 @@ class ActiveLearningCondorSubmitter(ProcessingPluginBase):
             self.logger.error(ex)
             self.logger.error(traceback.format_exc())
             raise exceptions.AgentPluginError('%s: %s' % (str(ex), traceback.format_exc()))
-
-    def get_job_dir(self, processing_id):
-        job_dir = 'processing_%s' % processing_id
-        job_dir = os.path.join(self.workdir, job_dir)
-        if not os.path.exists(job_dir):
-            os.makedirs(job_dir)
-        return job_dir
-
-    def generate_submit_script(self, processing_id, sandbox, executable, arguments, input_list, output_json):
-        script = "#!/bin/bash\n\n"
-        script += "sandbox=%s\n" % str(sandbox)
-        script += "executable=%s\n" % str(executable)
-        script += "arguments=%s\n" % str(arguments)
-        script += "input_list=%s\n" % str(input_list)
-        script += "output_json=%s\n" % str(output_json)
-        script += "\n"
-
-        script += "env\n"
-        script += "echo $X509_USER_PROXY\n"
-        script += "\n"
-
-        script += "wget $sandbox\n"
-        script += 'base_sandbox="$(basename -- $sandbox)"\n'
-        script += 'tar xzf $base_sandbox\n'
-        script += 'chmod +x $executable\n'
-        script += 'echo ./$executable $arguments\n'
-        script += './$executable $arguments\n'
-        script += '\n'
-
-        script_name = 'processing_%s.sh' % processing_id
-        script_name = os.path.join(self.get_job_dir(processing_id), script_name)
-        with open(script_name, 'w') as f:
-            f.write(script)
-        return script_name
-
-    def generate_submit_file(self, processing_id, sandbox, executable, arguments, input_list, output_json):
-        script_name = self.generate_submit_script(processing_id, sandbox, executable, arguments, input_list, output_json)
-
-        jdl = "#Agent jdl file\n"
-        jdl += "Universe        = vanilla\n"
-        jdl += "Notification    = Never\n"
-        jdl += "initialdir      = %s\n" % self.get_job_dir(processing_id)
-        jdl += "Executable      = %s\n" % script_name
-        # jdl += "Arguments       = %s\na" % (self.get_job_dir(processing_id))
-        jdl += "GetEnv          = False\n"
-        jdl += "Output          = " + 'processing_%s' % processing_id + ".$(ClusterId).$(ProcId).out\n"
-        jdl += "Error           = " + 'processing_%s' % processing_id + ".$(ClusterId).$(ProcId).err\n"
-        jdl += "Log             = " + 'processing_%s' % processing_id + ".$(ClusterId).$(ProcId).log\n"
-        jdl += "stream_output   = False\n"
-        jdl += "stream_error    = False\n"
-        # jdl += 'Requirements = ((Arch == "X86_64") && (regexp("SLC",OpSysLongName)))\n'
-        # jdl += 'Requirements = ((Arch == "X86_64") && (regexp("CentOS",OpSysLongName)))\n'
-        # jdl += "transfer_input_files = file1, file2\n"
-        if output_json:
-            jdl += "should_transfer_files = yes\n"
-            jdl += "transfer_output_files = %s\n" % output_json
-        jdl += "WhenToTransferOutput = ON_EXIT_OR_EVICT\n"
-        jdl += "OnExitRemove         = TRUE\n"
-        # jdl += '+JobFlavour = "espresso"\n'
-        # jdl += '+JobFlavour = "tomorrow"\n'
-        # jdl += '+JobFlavour = "testmatch"\n'
-        # jdl += '+JobFlavour = "nextweek"\n'
-        jdl += '+JobType="ActiveLearning"\n'
-        # jdl += '+AccountingGroup ="group_u_ATLASWISC.all"\n'
-        jdl += '+Processing_id = %s\n' % processing_id
-        jdl += "RequestCpus = 1\n"
-        if 'X509_USER_PROXY' in os.environ and os.environ['X509_USER_PROXY']:
-            jdl += "x509userproxy = %s\n" % str(os.environ['X509_USER_PROXY'])
-        jdl += "Queue 1\n"
-
-        submit_file = 'processing_%s.jdl' % processing_id
-        submit_file = os.path.join(self.get_job_dir(processing_id), submit_file)
-        with open(submit_file, 'w') as f:
-            f.write(jdl)
-        return submit_file
-
-    def submit_job(self, processing_id, sandbox, executable, arguments, input_list, output_json):
-        jdl_file = self.generate_submit_file(processing_id, sandbox, executable, arguments, input_list, output_json)
-        cmd = "condor_submit " + jdl_file
-        status, output, error = run_command(cmd)
-        jobid = None
-        self.logger.info("submiting the job to cluster: %s" % cmd)
-        self.logger.info("status: %s, output: %s, error: %s " % (status, output, error))
-        if status == 0 or str(status) == '0':
-            if output and 'submitted to cluster' in output:
-                for line in output.split('\n'):
-                    if 'submitted to cluster' in line:
-                        jobid = line.split(' ')[-1].replace('.', '')
-                        return jobid, None
-        return None, output + error
