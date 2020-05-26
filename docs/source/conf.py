aa437b9f0409e6880b4fb5dabbba5ea65a2e76fe
Wen Guan <wguan.icedew@gmail.com>
2020-05-26 15:16:01 +0200

    update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../common/lib/idds'))
-sys.path.insert(1, os.path.abspath('../../main/lib/idds'))
-sys.path.insert(2, os.path.abspath('../../client/lib/idds'))
-sys.path.insert(3, os.path.abspath('../../atlas/lib/idds'))
+# sys.path.insert(0, os.path.abspath('../../common/lib/idds'))
+# sys.path.insert(1, os.path.abspath('../../main/lib/idds'))
+# sys.path.insert(2, os.path.abspath('../../client/lib/idds'))
+# sys.path.insert(3, os.path.abspath('../../atlas/lib/idds'))
 
 # -- Project information -----------------------------------------------------
 
