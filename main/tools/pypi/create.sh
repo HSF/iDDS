97a770ec25a24a2a0b6b2c7e8a674915309c7f24
Wen Guan <wguan.icedew@gmail.com>
2021-03-28 01:40:07 +0100

    update dag docs

--- main/tools/pypi/create.sh
+++ main/tools/pypi/create.sh
@@ -2,5 +2,11 @@
 # create packages
 python setup.py sdist bdist_wheel
 
-python3 -m twine upload common/dist/idds-common-0.0.3.tar.gz 
-python3 -m twine upload client/dist/idds-client-0.0.3.tar.gz
+python3 -m twine upload  */dist/idds*-0.2.0.tar.gz
+
+python3 -m twine upload atlas/dist/idds-atlas-0.2.0.tar.gz
+python3 -m twine upload common/dist/idds-common-0.2.0.tar.gz
+python3 -m twine upload main/dist/idds-server-0.2.0.tar.gz
+python3 -m twine upload client/dist/idds-client-0.2.0.tar.gz
+python3 -m twine upload doma/dist/idds-doma-0.2.0.tar.gz
+python3 -m twine upload workflow/dist/idds-workflow-0.2.0.tar.gz
