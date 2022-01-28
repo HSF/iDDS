350ddc87e600dc745960594f1951353a6c8ded7d
Wen Guan <wguan.icedew@gmail.com>
2022-01-28 21:12:21 +0100

    fix missing packages

--- main/lib/idds/tests/test_domapanda.py
+++ main/lib/idds/tests/test_domapanda.py
@@ -40,7 +40,7 @@ from idds.doma.workflowv2.domapandawork import DomaPanDAWork
 task_queue = 'DOMA_LSST_GOOGLE_TEST'
 task_queue = 'DOMA_LSST_GOOGLE_MERGE'
 task_queue = 'SLAC_TEST'
-task_queue = 'DOMA_LSST_SLAC_TEST'
+# task_queue = 'DOMA_LSST_SLAC_TEST'
 
 
 def randStr(chars=string.ascii_lowercase + string.digits, N=10):
