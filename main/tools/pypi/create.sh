920f765cfd8d50c7368c70b7e18ce03d521b3069
Wen Guan <wguan.icedew@gmail.com>
2022-04-05 14:42:11 +0200

    update create pip script

--- main/tools/pypi/create.sh
+++ main/tools/pypi/create.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+
+version=$1
 
 # create packages
+echo python setup.py sdist bdist_wheel
 python setup.py sdist bdist_wheel
 
-python3 -m twine upload  */dist/idds*-0.2.0.tar.gz
+echo python3 -m twine upload  */dist/idds*-${version}.tar.gz
+python3 -m twine upload  */dist/idds*-${version}.tar.gz
 
-python3 -m twine upload atlas/dist/idds-atlas-0.2.0.tar.gz
-python3 -m twine upload common/dist/idds-common-0.2.0.tar.gz
-python3 -m twine upload main/dist/idds-server-0.2.0.tar.gz
-python3 -m twine upload client/dist/idds-client-0.2.0.tar.gz
-python3 -m twine upload doma/dist/idds-doma-0.2.0.tar.gz
-python3 -m twine upload workflow/dist/idds-workflow-0.2.0.tar.gz
+# python3 -m twine upload atlas/dist/idds-atlas-0.2.0.tar.gz
+# python3 -m twine upload common/dist/idds-common-0.2.0.tar.gz
+# python3 -m twine upload main/dist/idds-server-0.2.0.tar.gz
+# python3 -m twine upload client/dist/idds-client-0.2.0.tar.gz
+# python3 -m twine upload doma/dist/idds-doma-0.2.0.tar.gz
+# python3 -m twine upload workflow/dist/idds-workflow-0.2.0.tar.gz
