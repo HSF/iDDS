3b2827d79aa5db445d711a54e8b389d36b8735be
Wen Guan <wguan.icedew@gmail.com>
2022-09-06 15:56:42 +0200

    fix redis cache singleton

--- main/lib/idds/agents/common/cache/redis.py
+++ main/lib/idds/agents/common/cache/redis.py
@@ -26,10 +26,17 @@ class Singleton(object):
         return class_._instance
 
 
-class RedisCache(Singleton):
+class RedisCache(object):
     """
     Redis cache
     """
+
+    _instance = None
+
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance
 
     def __init__(self, logger=None):
         super(RedisCache, self).__init__()
