8660fd81e64110639d57a203239b0e878e48af81
Wen Guan <wguan.icedew@gmail.com>
2022-10-01 13:03:39 +0200

    fix timedelta json dumps

--- common/lib/idds/common/utils.py
+++ common/lib/idds/common/utils.py
@@ -350,6 +350,8 @@ class DictClassEncoder(json.JSONEncoder):
             return obj.to_dict()
         elif isinstance(obj, datetime.datetime):
             return date_to_str(obj)
+        elif isinstance(obj, datetime.timedelta):
+            return str(obj)
         # elif isinstance(obj, (datetime.time, datetime.date)):
         #     return obj.isoformat()
         # elif isinstance(obj, datetime.timedelta):
