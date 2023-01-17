27d805832955ff0c6914ff6f0b46bba8e19059ff
Wen Guan <wguan.icedew@gmail.com>
2023-01-17 11:01:59 +0100

    update docker syslog-ng

--- start-daemon.sh
+++ start-daemon.sh
@@ -199,6 +199,7 @@ else
 fi
 
 echo "start syslog-ng"
-/usr/sbin/syslog-ng --no-caps
+# /usr/sbin/syslog-ng -F --no-caps --persist-file=/var/log/idds/syslog-ng.persist -p /var/log/idds/syslog-ng.pid
+/usr/sbin/syslog-ng  --no-caps --persist-file=/var/log/idds/syslog-ng.persist -p /var/log/idds/syslog-ng.pid
 
 trap : TERM INT; sleep infinity & wait
