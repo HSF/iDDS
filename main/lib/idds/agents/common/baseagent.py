4a47efc1a7975b9d77989c8fdf0711206c883e24
Wen Guan <wguan.icedew@gmail.com>
2023-03-31 14:09:04 +0200

    fix heartbeat to check pids and optimize event scheduler

--- main/lib/idds/agents/common/baseagent.py
+++ main/lib/idds/agents/common/baseagent.py
@@ -17,10 +17,11 @@ import uuid
 
 from idds.common.constants import Sections
 from idds.common.constants import (MessageType, MessageTypeStr,
-                                   MessageStatus, MessageSource)
+                                   MessageStatus, MessageSource,
+                                   ReturnCode)
 from idds.common.plugin.plugin_base import PluginBase
 from idds.common.plugin.plugin_utils import load_plugins, load_plugin_sequence
-from idds.common.utils import setup_logging
+from idds.common.utils import setup_logging, pid_exists, json_dumps
 from idds.core import health as core_health, messages as core_messages
 from idds.agents.common.timerscheduler import TimerScheduler
 from idds.agents.common.eventbus.eventbus import EventBus
@@ -41,6 +42,9 @@ class BaseAgent(TimerScheduler, PluginBase):
         self.id = str(uuid.uuid4())[:8]
         self.logger = logger
         self.setup_logger(self.logger)
+
+        self.thread_id = None
+        self.thread_name = None
 
         self.config_section = Sections.Common
 
@@ -86,6 +90,17 @@ class BaseAgent(TimerScheduler, PluginBase):
     def get_name(self):
         return self.name
 
+    def init_thread_info(self):
+        hb_thread = threading.current_thread()
+        self.thread_id = hb_thread.ident
+        self.thread_name = hb_thread.name
+
+    def get_thread_id(self):
+        return self.thread_id
+
+    def get_thread_name(self):
+        return self.thread_name
+
     def load_agent_attributes(self, kwargs):
         rets = {}
         for key in kwargs:
@@ -124,28 +139,43 @@ class BaseAgent(TimerScheduler, PluginBase):
     def execute_event_schedule(self):
         event_ids = list(self.event_futures.keys())
         for event_id in event_ids:
-            event, future = self.event_futures[event_id]
+            event, future, start_time = self.event_futures[event_id]
             if future.done():
                 ret = future.result()
+                status = "finished"
+                end_time = time.time()
                 if ret is None or ret == 0:
                     self.event_bus.clean_event(event)
+                elif ret == ReturnCode.Locked.value:
+                    status = "locked"
+                    self.event_bus.fail_event(event)
                 else:
+                    status = "failed"
                     self.event_bus.fail_event(event)
                 del self.event_futures[event_id]
+                self.event_bus.send_report(event, status, start_time, end_time, self.get_hostname(), ret)
+                if status == 'locked':
+                    self.logger.warning("Corresponding resource is locked, put the event back again: %s" % json_dumps(event))
+                    event.requeue()
+                    self.event_bus.send(event)
 
         event_funcs = self.get_event_function_map()
         for event_type in event_funcs:
             exec_func = event_funcs[event_type]['exec_func']
-            pre_check = event_funcs[event_type]['pre_check']
+            # pre_check = event_funcs[event_type]['pre_check']
             to_exec_at = event_funcs[event_type].get("to_exec_at", None)
             if to_exec_at is None or to_exec_at < time.time():
-                if pre_check():
+                # if pre_check():
+                if self.executors.has_free_workers():
                     event = self.event_bus.get(event_type)
                     if event:
                         future = self.executors.submit(exec_func, event)
-                        self.event_futures[event._id] = (event, future)
-                    else:
-                        event_funcs[event_type].get("to_exec_at", time.time() + self.event_interval_delay)
+                        self.event_futures[event._id] = (event, future, time.time())
+                event_funcs[event_type]["to_exec_at"] = time.time() + self.event_interval_delay
+
+    def execute_schedules(self):
+        self.execute_timer_schedule()
+        self.execute_event_schedule()
 
     def execute(self):
         while not self.graceful_stop.is_set():
@@ -163,28 +193,69 @@ class BaseAgent(TimerScheduler, PluginBase):
         try:
             self.logger.info("Starting main thread")
 
+            self.init_thread_info()
             self.load_plugins()
 
             self.execute()
         except KeyboardInterrupt:
             self.stop()
-            self.event_bus.stop()
 
     def __call__(self):
         self.run()
 
+    def stop(self):
+        super(BaseAgent, self).stop()
+        self.event_bus.stop()
+
     def terminate(self):
         self.stop()
 
-    def health_heartbeat(self):
+    def get_hostname(self):
+        hostname = socket.getfqdn()
+        return hostname
+
+    def is_self(self, health_item):
         hostname = socket.getfqdn()
         pid = os.getpid()
-        hb_thread = threading.current_thread()
-        thread_id = hb_thread.ident
-        thread_name = hb_thread.name
+        thread_id = self.get_thread_id()
+        ret = False
+        if ('hostname' in health_item and 'pid' in health_item and 'agent' in health_item
+            and 'thread_id' in health_item and health_item['hostname'] == hostname        # noqa W503
+            and health_item['pid'] == pid and health_item['agent'] == self.get_name()     # noqa W503
+            and health_item['thread_id'] == thread_id):                                              # noqa W503
+            ret = True
+        if not ret:
+            self.logger.debug("is_self: hostname %s, pid %s, thread_id %s, agent %s, health %s" % (hostname, pid, thread_id, self.get_name(), health_item))
+        return ret
+
+    def get_health_payload(self):
+        return None
+
+    def health_heartbeat(self, heartbeat_delay=None):
+        if heartbeat_delay:
+            self.heartbeat_delay = heartbeat_delay
+        hostname = socket.getfqdn()
+        pid = os.getpid()
+        thread_id = self.get_thread_id()
+        thread_name = self.get_thread_name()
+        payload = self.get_health_payload()
+        self.logger.debug("health heartbeat: agent %s, pid %s, thread %s, delay %s" % (self.get_name(), pid, thread_name, self.heartbeat_delay))
         core_health.add_health_item(agent=self.get_name(), hostname=hostname, pid=pid,
-                                    thread_id=thread_id, thread_name=thread_name, payload=None)
+                                    thread_id=thread_id, thread_name=thread_name, payload=payload)
         core_health.clean_health(older_than=self.heartbeat_delay * 2)
+
+        health_items = core_health.retrieve_health_items()
+        pids, pid_not_exists = [], []
+        for health_item in health_items:
+            if health_item['hostname'] == hostname:
+                pid = health_item['pid']
+                if pid not in pids:
+                    pids.append(pid)
+        for pid in pids:
+            if not pid_exists(pid):
+                pid_not_exists.append(pid)
+        if pid_not_exists:
+            core_health.clean_health(hostname=hostname, pids=pid_not_exists, older_than=None)
 
     def add_default_tasks(self):
         task = self.create_task(task_func=self.health_heartbeat, task_output_queue=None,
