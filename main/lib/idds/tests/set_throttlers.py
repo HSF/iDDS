e02de624fbc6e7ea3afc1a6fa42956b9994d230c
Wen Guan <wguan.icedew@gmail.com>
2023-05-21 16:40:07 +0200

    add set throttler func

---
+++ main/lib/idds/tests/set_throttlers.py
@@ -0,0 +1,8 @@
+from idds.common.constants import ThrottlerStatus
+from idds.core import throttlers as core_throttlers
+
+throttler = {'site': 'Default',
+             'status': ThrottlerStatus.Active,
+             'new_contents': 100000,
+             'queue_contents': 10000}
+core_throttlers.add_throttler(**throttler)
