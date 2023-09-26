615ff269f3e0c6d86f466505cadbffafefcbb2b2
Wen Guan <wguan.icedew@gmail.com>
2023-09-26 22:12:08 +0200

    fix messaging sender

--- main/etc/sql/postgresql_update.sql
+++ main/etc/sql/postgresql_update.sql
@@ -3,3 +3,7 @@ alter table health add column status INTEGER;
 alter table contents_update add column content_metadata VARCHAR(100);
 alter table contents_update add column fetch_status INTEGER DEFAULT 0;
 
+
+-- 2023.09.26
+-- update slac idds database, without updating the idds models
+alter table contents alter column name type varchar(8000);
