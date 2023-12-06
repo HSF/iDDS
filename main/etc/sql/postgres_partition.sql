137d72eec62281bd2662f7a59b3f9f679b9f6288
Edward Karavakis <edward.karavakis@cern.ch>
2023-12-06 14:30:31 +0100

    Changed the interval to 1K after discussing it with Wen.

--- main/etc/sql/postgres_partition.sql
+++ main/etc/sql/postgres_partition.sql
@@ -2,6 +2,6 @@ SELECT partman.create_parent(
 p_parent_table => 'doma_idds.contents',
 p_control => 'request_id',
 p_type => 'native',
-p_interval=> '1000000',
+p_interval=> '1000',
 p_premake => 3
 );
