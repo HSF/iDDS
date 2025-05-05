06c5c323324e2d19bfe6f8fa1a0e0d6c64337b30
Wen Guan <wguan.icedew@gmail.com>
2025-05-05 15:27:28 +0200

    fix task polling

--- monitor/data/conf.js
+++ monitor/data/conf.js
@@ -1,9 +1,9 @@
 
 var appConfig = {
-    'iddsAPI_request': "https://lxplus972.cern.ch:443/idds/monitor_request/null/null",
-    'iddsAPI_transform': "https://lxplus972.cern.ch:443/idds/monitor_transform/null/null",
-    'iddsAPI_processing': "https://lxplus972.cern.ch:443/idds/monitor_processing/null/null",
-    'iddsAPI_request_detail': "https://lxplus972.cern.ch:443/idds/monitor/null/null/true/false/false",
-    'iddsAPI_transform_detail': "https://lxplus972.cern.ch:443/idds/monitor/null/null/false/true/false",
-    'iddsAPI_processing_detail': "https://lxplus972.cern.ch:443/idds/monitor/null/null/false/false/true"
+    'iddsAPI_request': "https://lxplus901.cern.ch:443/idds/monitor_request/null/null",
+    'iddsAPI_transform': "https://lxplus901.cern.ch:443/idds/monitor_transform/null/null",
+    'iddsAPI_processing': "https://lxplus901.cern.ch:443/idds/monitor_processing/null/null",
+    'iddsAPI_request_detail': "https://lxplus901.cern.ch:443/idds/monitor/null/null/true/false/false",
+    'iddsAPI_transform_detail': "https://lxplus901.cern.ch:443/idds/monitor/null/null/false/true/false",
+    'iddsAPI_processing_detail': "https://lxplus901.cern.ch:443/idds/monitor/null/null/false/false/true"
 }
