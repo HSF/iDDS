0bea9dd748d67390625b08296ebc090917261d9a
Wen Guan <wguan.icedew@gmail.com>
2026-03-27 15:41:35 +0100

    optimize fixiing missing deps

--- main/lib/idds/agents/carrier/utils.py
+++ main/lib/idds/agents/carrier/utils.py
@@ -712,10 +712,12 @@ def handle_new_processing(processing, agent_attributes, func_site_to_cloud=None,
             wait_futures_finish(ret_futures, "handle_new_processing", logger, log_prefix)
 
     # fix missing content_dep_id for input_dependency contents
-    core_processings.update_processing_contents(update_processing=None,
-                                                request_id=request_id,
-                                                transform_id=transform_id,
-                                                fix_missing_content_dep_id=True)
+    ret_fix = core_processings.update_processing_contents(update_processing=None,
+                                                         request_id=request_id,
+                                                         transform_id=transform_id,
+                                                         fix_missing_content_dep_id=True)
+    num_added_contents, num_updated_contents, num_added_messages, num_updated_messages = ret_fix
+    logger.debug(log_prefix + "handle_new_processing: fix missing content_dep_id for input_dependency contents, num_fixed: %s" % num_updated_contents)
 
     logger.debug(log_prefix + "handle_new_processing: finish")
 
