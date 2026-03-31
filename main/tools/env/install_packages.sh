ae1f3dc65cc0c43fa2aec25f11bdbc265b4cdcb2
Wen Guan <wguan.icedew@gmail.com>
2026-03-31 17:01:06 +0200

    fix docker installation

--- main/tools/env/install_packages.sh
+++ main/tools/env/install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -m
-for package in common workflow client main doma atlas website monitor ;
+for package in common workflow client main doma atlas website monitor prompt ;
 do
   python3 -m pip install $package/dist/*.whl 2>/dev/null || python3 -m pip install $package/dist/*.tar.gz
 done
