a334b225b755703e14b54bcc58480bd6c12c0083
Wen Guan <wguan.icedew@gmail.com>
2026-05-15 13:03:21 +0200

    fix run workflow wrapper

--- workflow/tools/workflow/make/make.sh
+++ workflow/tools/workflow/make/make.sh
@@ -27,7 +27,7 @@ source $workdir/bin/activate
 echo "install panda client"
 pip install panda-client
 # pip install tabulate requests urllib3==1.26.18 argcomplete packaging anytree networkx stomp.py==8.0.1
-pip install tabulate requests urllib3 argcomplete packaging anytree networkx stomp.py wheel
+pip install tabulate requests "urllib3<2" argcomplete packaging anytree networkx stomp.py wheel
 
 echo "install idds-common"
 pip install ${RootDir}/common
